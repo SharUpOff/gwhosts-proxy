f0986514c7b227385d9f80bdec36373608b81bda
SharUpOff <sharupoff@efstudios.org>
2024-09-15 19:23:25 +0400

    [fix] Update answers count when removing IPv6 answers

--- gwhosts/dns/_tools.py
+++ gwhosts/dns/_tools.py
@@ -1,11 +1,21 @@
-from gwhosts.dns._types import DNSData, RRType
+from gwhosts.dns._types import DNSData, RRType, Header
 
 
 def remove_ipv6(data: DNSData) -> DNSData:
+    answers = [answer for answer in data.answers if answer.rr_type != RRType.AAAA.value]
+    header = data.header
+
     return DNSData(
-        header=data.header,
+        header=Header(
+            id=header.id,
+            flags=header.flags,
+            questions=header.questions,
+            answers=len(answers),
+            authorities=header.authorities,
+            additions=header.additions,
+        ),
         questions=data.questions,
-        answers=[answer for answer in data.answers if answer.rr_type != RRType.AAAA.value],
+        answers=answers,
         authorities=data.authorities,
         additions=data.additions,
     )
